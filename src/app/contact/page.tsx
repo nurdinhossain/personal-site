9193af29de7c2a6780b3ba183455795a553f4753
Nurdin <nurdinhossain@gmail.com>
2024-05-27 23:25:48 -0400

    Create contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export type FormData = {
     name: string,
@@ -10,8 +11,25 @@ export type FormData = {
 
 export default function Contact() {
     const { register, handleSubmit } = useForm<FormData>();
+    const [data, setData] = useState("");
 
     return (
-        <p>Hi</p>
+        <div>
+            {/* header */}
+            <h1 className="bg-green-300 border-4 border-dashed border-slate-900 font-bold text-center text-4xl m-6 p-2">Contact</h1>
+
+            <main className="flex flex-row justify-center">
+                <form name="ContactForm" className="flex flex-col bg-cyan-300 border-4 border-dashed border-slate-900 space-y-10 w-5/12 m-5 p-4" onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))} data-netlify="true">
+                    <input {...register("name")} placeholder="Full name" className="h-12 border-4 border-solid border-cyan-700 p-2" />
+                    <input {...register("email")} placeholder="Email address" className="h-12 border-4 border-solid border-cyan-700 p-2" />
+                    <textarea {...register("message")} placeholder="Message" className="h-80 border-4 border-solid border-cyan-700 p-2" />
+                    {
+                        data == "" ? 
+                        <input type="submit" className="bg-indigo-400 border-4 border-solid border-indigo-700 text-2xl" /> : 
+                        <div className="bg-green-400 border-4 border-solid border-green-700 text-2xl text-center">Sent!</div>
+                    }
+                </form>
+            </main>
+        </div>
     )
 }
